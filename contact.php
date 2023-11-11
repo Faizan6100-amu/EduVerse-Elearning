0c7e66d6a865b3ddd3c8a8b91a995daeb1808b60
Faizan Anwar Khan <139392689+Faizan6100-amu@users.noreply.github.com>
2023-11-11 15:41:13 +0530

    All file added

---
+++ contact.php
@@ -0,0 +1,25 @@
+<!--Start Contact Us-->
+<div class="container mt-4" id="Contact">  <!--Start Contact Us Container-->
+      <h2 class="text-center mb-4">Contact US</h2> <!-- Contact Us Heading -->
+      <div class="row">  <!--Start Contact Us Row-->
+        <div class="col-md-8"> <!--Start Contact Us 1st Column-->
+          <form action="" method="post">
+            <input type="text" class="form-control" name="name" placeholder="Name"><br>
+            <input type="text" class="form-control" name="subject" placeholder="Subject"><br>
+            <input type="email" class="form-control" name="email" placeholder="E-mail"><br>
+            <textarea class="form-control" name="message" placeholder="How can we help you?" style="height:150px;"></textarea><br>
+            <input class="btn btn-primary" type="submit" value="Send" name="submit"><br><br>
+          </form>
+        </div> <!-- End Contact Us 1st Column-->
+
+        <div class="col-md-4 stripe text-white text-center">  <!-- Start Contact Us 2nd Column-->
+          <h4>EduVerse</h4>
+          <p>An E-Learning Portal, 
+           ALigarh, 
+          Uttar Pradesh - 202002<br /> 
+          Phone: +9161****31 <br />
+          www.EduVerse.com </p>
+        </div> <!-- End Contact Us 2nd Column-->
+      </div> <!-- End Contact Us Row-->
+    </div> <!-- End Contact Us Container-->
+    <!-- End Contact Us -->
